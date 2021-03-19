9e73b1ae3f30b584a84039e5193fb4213e163bb2
sangya2001 <sangyasherpa2058@gmail.com>
2021-03-19 08:44:08 +0545

    Completed Normal File Upload with Security Stuff.

--- controller/upload.js
+++ controller/upload.js
@@ -1,20 +1,29 @@
 const multer = require('multer');
 const express = require('express');
 const UploadRouter = express.Router();
+const sha1 = require('sha1')
 
 // @dev uploads/ may not work on all OS (I'm using Windows 10 Home Edition) so you can use file path to navigate to the folder
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'uploads');
     },
     filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, fileName)
+        // @dev sha1 is used to hash the fileName to mitigate FMA(File Metadata Attack)
+        const fileName = sha1(file.originalname.toLowerCase().split(' ').join('-'))
+        const fileMimeType = file.originalname.toLowerCase().split('.');
+
+        const fName = fileName + '.' + fileMimeType[fileMimeType.length - 1]
+        cb(null, fName)
     }
 });
 
+// @dev file of maximum 10 mb size can be uploaded to mitigate the file size attack
+const maxFileSize = 10 * 1000 * 1000;
+
 var upload = multer({
     storage: storage,
+    limits: { fileSize: maxFileSize },
     // @dev instead of OR operation in if conditionals,  we can use switch operation too.
     fileFilter: (req, file, cb) => {
         if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
